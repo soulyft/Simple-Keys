5e3c2e05e6cafc3fe24f6e74554ad0483fdea445
Corey Lofthus <corey@soulyftaudio.com>
2025-10-01 23:43:02 -0700

    first commit, basic "mini" or 5 key layout view

--- Simple Keys/ContentView.swift
+++ Simple Keys/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let offset: CGFloat = 8
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            
+            //rectangles are keys
+            HStack(spacing: 8) {
+                Spacer()
+                    .frame(width: 64)
+                Rectangle()
+                    .foregroundStyle(.black.opacity(0.88))
+                Rectangle()
+                    .foregroundStyle(.black.opacity(0.88))
+                Spacer()
+                    .frame(width: 64)
+            }
+            
+            //spaces are keys
+            HStack(spacing: 16) {
+                Spacer()
+                    .frame(width: 120 - offset)
+                Rectangle()
+                    .foregroundStyle(.black.opacity(0.88))
+                Spacer()
+                    .frame(width: 88 + offset)
+                Rectangle()
+                    .foregroundStyle(.black.opacity(0.88))
+                Spacer()
+                    .frame(width: 120 - offset)
+            }
+                .frame(height: 256)
         }
-        .padding()
+        .background{
+            Color.black.opacity(0.08)
+        }
+        .ignoresSafeArea()
+        
     }
 }
 
