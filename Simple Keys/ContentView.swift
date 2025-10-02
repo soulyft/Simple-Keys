420962446e604b2bc41e7365a94dac22c57eab35
Corey Lofthus <corey@soulyftaudio.com>
2025-10-01 22:55:51 -0700

    Initial Commit

---
+++ Simple Keys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Keys
+//
+//  Created by Corey Lofthus on 10/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Keys/Simple_KeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_KeysApp.swift
+//  Simple Keys
+//
+//  Created by Corey Lofthus on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Simple_KeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
